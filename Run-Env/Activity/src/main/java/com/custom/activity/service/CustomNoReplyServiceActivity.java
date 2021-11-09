0e52cec56a4e5e3b293d1406bd0035631ad07b7f
jaesuk <jaesuk@inzent.com>
2021-11-09 14:44:13 +0900

    무응답 및 클러스터를 이용한 동기 서비스 추가

---
+++ Run-Env/Activity/src/main/java/com/custom/activity/service/CustomNoReplyServiceActivity.java
@@ -0,0 +1,25 @@
+package com.custom.activity.service ;
+
+import com.custom.activity.telegram.CustomHandlerConstants ;
+import com.inzent.igate.activity.service.NoReplyServiceActivity ;
+import com.inzent.igate.adapter.AdapterParameter ;
+import com.inzent.igate.exception.IGateException ;
+import com.inzent.igate.message.Record ;
+import com.inzent.igate.repository.meta.Activity ;
+import com.inzent.igate.repository.meta.Service ;
+
+public class CustomNoReplyServiceActivity extends NoReplyServiceActivity implements CustomHandlerConstants
+{
+  public CustomNoReplyServiceActivity(Activity meta)
+  {
+    super(meta) ;
+  }
+
+  @Override
+  protected AdapterParameter makeAdapterParameter(Service serviceMeta, Record request) throws IGateException
+  {
+    request.setFieldValue(SYNC_MODE_PATH, "N") ;
+
+    return super.makeAdapterParameter(serviceMeta, request) ;
+  }
+}
