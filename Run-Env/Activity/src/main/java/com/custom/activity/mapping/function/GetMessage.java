0ba130c8be42ef927a4482a91db133f72aa1e138
jokyehwan <jokyehwan88@inzent.com>
2022-04-04 15:05:14 +0900

    Mapping Test

---
+++ Run-Env/Activity/src/main/java/com/custom/activity/mapping/function/GetMessage.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * This program and the accompanying materials are made
+ * available under the terms of the Inzent MCA License v1.0
+ * which accompanies this distribution.
+ * 
+ * Contributors:
+ *     Inzent Corporation - initial API and implementation
+ *******************************************************************************/
+package com.custom.activity.mapping.function ;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.inzent.igate.exception.IGateException ;
+import com.inzent.igate.mapping.operator.AbstractExpressionFunction ;
+import com.inzent.igate.repository.meta.Activity ;
+import com.inzent.igate.util.message.MessageTranslator;
+
+public class GetMessage extends AbstractExpressionFunction
+{
+	public GetMessage(Activity meta)
+	{
+		super(meta) ;
+	}
+
+	@Override
+	public boolean isSingleton()
+	{
+		return true ;
+	}
+
+	@Override
+	public int getArgumentCount()
+	{
+		return 3 ;
+	}
+
+	@Override
+	protected Object doExecute(Object... args) throws IGateException
+	{
+		String langCd 	= ( null!= args[0] ? (String) args[0] : "en" );
+		String resCd 	= ( null!= args[1] ? (String) args[1] : null );
+		String adapterId = ( null!= args[2] ? (String) args[2] : "I_MCA" );
+
+		int idx = 0;
+		String retMessage = "";
+
+		if(resCd != null)	
+		{	
+			for (String message : MessageTranslator.getStandardMessage(resCd, adapterId, langCd))
+			{
+				if (StringUtils.isBlank(message))
+					break ;
+
+				if (0 != idx)
+					retMessage += " , ";
+				retMessage += message ;
+
+				idx ++;
+			}
+		}
+		return retMessage;
+	}
+}
+
