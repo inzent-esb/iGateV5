6773560c1004d9fddcccdca8b30fd7b50b7c6c68
jokyehwan <jokyehwan88@inzent.com>
2022-07-18 15:06:28 +0900

    대외온라인 타발 비동기비동기 구성의 TID 생성용 엑티비티

---
+++ Run-Env/Activity/src/main/java/com/inzent/igate/activity/adapter/GenTid.java
@@ -0,0 +1,40 @@
+package com.inzent.igate.activity.adapter ;
+
+import com.inzent.igate.context.TransactionContextBean ;
+import com.inzent.igate.exception.IGateException ;
+import com.inzent.igate.repository.meta.Activity ;
+import com.inzent.igate.rule.activity.AbstractActivity ;
+
+public class GenTid extends AbstractActivity
+{
+  public GenTid(Activity meta)
+  {
+    super(meta) ;
+  }
+
+  @Override
+  public boolean isSingleton()
+  {
+    return true ;
+  }
+
+  /**
+   * 
+   * @return
+   * @throws IGateException
+   * @author jkh, 2021. 12. 17.
+   */
+  @Override
+  public int execute(Object... args) throws IGateException
+  {    
+//    String date17 = com.inzent.igate.util.PatternUtils.dateFormatFull.format(java.lang.System.currentTimeMillis());
+//    String tid = date17 + java.util.UUID.randomUUID().toString().replace("-", "");
+//    tid += "_INZT_TID" ;
+    String tid = java.util.UUID.randomUUID().toString();
+    logger.debug("# TID : [" + tid + "]");
+    
+    TransactionContextBean.getInstance().setValue("TID", tid ) ;
+    
+    return 0 ;
+  }
+}
