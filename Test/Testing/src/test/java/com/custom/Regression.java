1ad62e3fe4ff737c34c2b220a0a03373def70b11
jokyehwan <jokyehwan88@inzent.com>
2024-02-15 11:47:32 +0900

    삭제

--- Test/Testing/src/test/java/com/custom/Regression.java
+++ Test/Testing/src/test/java/com/custom/Regression.java
@@ -4,24 +4,21 @@ public interface Regression
 {
   public static final String TRUNCATE_SQL = "TRUNCATE TABLE %s";
   
-  public static final String ASYNC_VALID_JDBC_URL = "jdbc:mysql://218.50.65.12/igate5?useUnicode=true&serverTimezone=UTC";
-  //public static final String COR_JDBC_URL = "jdbc:mysql://192.168.20.201/igate5?useUnicode=true&serverTimezone=UTC";
+  public static final String ASYNC_VALID_JDBC_URL ="";
   public static final String ASYNC_VALID_JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-  public static final String ASYNC_VALID_JDBC_ID = "igate5";
-  public static final String ASYNC_VALID_JDBC_PASSWORD = "igate5";
+  public static final String ASYNC_VALID_JDBC_ID ="";
+  public static final String ASYNC_VALID_JDBC_PASSWORD ="";
   public static final String ASYNC_VALID_FLAG = "T";
   
-  public static final String COR_JDBC_URL = "jdbc:mysql://218.50.65.12/eai?useUnicode=true&serverTimezone=UTC";
-  //public static final String COR_JDBC_URL = "jdbc:mysql://192.168.20.201/igate5?useUnicode=true&serverTimezone=UTC";
+  public static final String COR_JDBC_URL ="";
   public static final String COR_JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-  public static final String COR_JDBC_ID = "eai";
-  public static final String COR_JDBC_PASSWORD = "inzent";
+  public static final String COR_JDBC_ID ="";
+  public static final String COR_JDBC_PASSWORD ="";
   
-  public static final String EDW_JDBC_URL = "jdbc:oracle:thin:@//218.50.65.12/orcl";
-  //public static final String EDW_JDBC_URL = "jdbc:oracle:thin:@//192.168.20.201/orcl";
+  public static final String EDW_JDBC_URL ="";
   public static final String EDW_JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";
-  public static final String EDW_JDBC_ID = "EAI";
-  public static final String EDW_JDBC_PASSWORD = "inzent";
+  public static final String EDW_JDBC_ID ="";
+  public static final String EDW_JDBC_PASSWORD ="";
   
   public static final String CONNECTOR_ADDRESS = "tuto.inzent.com" ;
   public static final int TER_CONNECTOR_PORT = 5001 ;
@@ -35,11 +32,10 @@ public interface Regression
   public static final int KFTC_API_CONNECTOR_PORT = 6080 ;
   public static final int TIMEOUT = 10000 ;
 
-  public static final String ECHO_CONNECTOR_ADDRESS = "218.50.65.12" ;
-//  public static final String ECHO_CONNECTOR_ADDRESS = "192.168.20.202" ;
+  public static final String ECHO_CONNECTOR_ADDRESS ="";
   public static final int ECHO_KFB_TAX_CONNECTOR_PORT = 6113 ;
-  public static final String echo_username ="igtecho";
-  public static final String echo_password = "inzent";
+  public static final String echo_username ="";
+  public static final String echo_password ="";
 
   public static final int DATA_LEN_LENGTH = 8 ;
   public static final int RESPONSE_CODE_OFFSET = 149 ;
@@ -58,8 +54,8 @@ public interface Regression
   public static final String localWriteFileDir = "/home/igate5/iGate/local/write";
   public static final String[] localFileList = new String[] { "IF_MCA_COM_0130_01S.dat" };
   public static final String[] sftpFileList = new String[] { "IF_COM_MCA_0131_01S.dat" };
-  public static final String username ="igate5";
-  public static final String password = "inzent";
+  public static final String username ="";
+  public static final String password ="";
   public static final int ftpPort = 22;
   
 }
